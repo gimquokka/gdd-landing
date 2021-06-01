8cf843fe73c3ea4479b9aa95c81c94649a99d9e1
chorom-ham <chorom.m.ham@gmail.com>
2021-06-01 10:30:46 +0900

    💄 style: Add colors

---
+++ src/layouts/constants/colors.ts
@@ -0,0 +1,13 @@
+export const WHITE = "#FFFFFF";
+export const BLACK = "#333333";
+export const GREY = {
+  100: "#fbfbfb",
+  500: "#848494",
+  600: "#7e7891",
+  800: "#342d54",
+};
+export const GREEN = "#10d583";
+export const LIGHT_GREEN = "#e2f7e5";
+export const NAVY = "#e2f7e5";
+export const SKY_BLUE = "#57c8c9";
+export const BLUE = "#2589a7";
