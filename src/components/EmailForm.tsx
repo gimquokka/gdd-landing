a5ce2b654dd66bbb1cb0b97676e20c8cce61faa6
chorom-ham <chorom.m.ham@gmail.com>
2021-06-01 15:37:29 +0900

    ✨ feat: Add email form warning text

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -4,10 +4,15 @@ import { WHITE, GREEN, GREY } from "src/constants/colors";
 
 export default function EmailForm() {
   return (
-    <Form>
-      <Input type="email" placeholder="이메일을 입력해주세요" required />
-      <Button type="submit">신청</Button>
-    </Form>
+    <>
+      <Form>
+        <Input type="email" placeholder="이메일을 입력해주세요" required />
+        <Button type="submit">신청</Button>
+      </Form>
+      <Text>
+        신청 시, 홈써치 구독 신청과 개인정보 수집에 동의하는 것으로 간주합니다
+      </Text>
+    </>
   );
 }
 
@@ -33,4 +38,10 @@ const Button = styled.button`
   background-color: ${GREEN};
   border: none;
   border-radius: 0.4rem;
+`;
+
+const Text = styled.p`
+  padding: 0.2rem;
+  font-size: 1rem;
+  color: ${GREY[500]};
 `;
