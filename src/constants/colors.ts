8b0a487c2fe252713d67752e5c61a3bfa82b6e45
chorom-ham <chorom.m.ham@gmail.com>
2021-06-01 15:30:10 +0900

    💄 style: Fix navy color's hex code

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -8,6 +8,6 @@ export const GREY = {
 };
 export const GREEN = "#10d583";
 export const LIGHT_GREEN = "#e2f7e5";
-export const NAVY = "#e2f7e5";
+export const NAVY = "#3c42ce";
 export const SKY_BLUE = "#57c8c9";
 export const BLUE = "#2589a7";
