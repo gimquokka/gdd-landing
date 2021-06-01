5b0fd8b6cbb29abf3dba1ab81a0eee67be2d5e31
chorom-ham <chorom.m.ham@gmail.com>
2021-06-01 15:33:25 +0900

    ✨ feat: Add email form

---
+++ src/components/EmailForm.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+import { WHITE, GREEN, GREY } from "src/constants/colors";
+
+export default function EmailForm() {
+  return (
+    <Form>
+      <Input type="email" placeholder="이메일을 입력해주세요" required />
+      <Button type="submit">신청</Button>
+    </Form>
+  );
+}
+
+const Form = styled.form`
+  display: flex;
+`;
+
+const Input = styled.input`
+  width: 24rem;
+  padding: 0.6rem 1.2rem;
+  font-size: 1.8rem;
+  background-color: ${GREY[100]};
+  border: 1px solid ${GREY[500]};
+  border-radius: 0.4rem;
+`;
+
+const Button = styled.button`
+  padding: 0.6rem 1.2rem;
+  margin-left: 1rem;
+  font-size: 1.8rem;
+  font-weight: 500;
+  color: ${WHITE};
+  background-color: ${GREEN};
+  border: none;
+  border-radius: 0.4rem;
+`;
