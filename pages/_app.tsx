225a9c0ab3160d6bc4eace71a8952e7d3b123cf8
chorom-ham <chorom.m.ham@gmail.com>
2021-06-02 14:21:09 +0900

    💄 style: Fix main width (Remove x-scroll)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 export default MyApp;
 
 const Main = styled.main`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   min-height: 100vh;
   position: relative;
